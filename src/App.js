98e1834deda8330f86b824b282f364bbc5dff1f2
KhulupArtemy <artem.khulup@gmail.com>
2023-04-07 18:14:32 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
 
 function App() {
     const {onToggleButton, tg} = useTelegram();
@@ -9,11 +10,12 @@ function App() {
         tg.ready();
     }, [])
 
-  return (
+    return (
     <div className="App">
-      <button onClick={onToggleButton}>toggle</button>
+        <Header/>
+        <button onClick={onToggleButton}>toggle</button>
     </div>
-  );
+    );
 }
 
 export default App;
